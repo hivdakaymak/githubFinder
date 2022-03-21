b6ff1749aae8956d8f051d993565bbb891d49483
miraykaymak <miirayazz00@gmail.com>
2022-03-21 14:52:58 +0300

    Error message

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,60 @@
-import React, { Component, Fragment } from "react";
-import Navbar from "./Navbar";
-import Users from "./Users";
-import axios from "axios";
-import Search from "./Search";
+import React, { Component, Fragment } from 'react'
+import Navbar from './Navbar'
+import Users from './Users'
+import Search from './Search'
+import Alert from './Alert'
+import axios from 'axios'
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.searchUsers = this.searchUsers.bind(this);
-    this.clearUsers = this.clearUsers.bind(this);
+    constructor(props) {
+        super(props);
+        this.searchUsers = this.searchUsers.bind(this);
+        this.clearUsers = this.clearUsers.bind(this);
+        this.setAlert = this.setAlert.bind(this);
+        this.state = {
+            loading: false,
+            users: [],
+            alert: null
+        }
+    }
 
-    this.state = {
-      loading: false,
-      users: [],
-    };
-  }
+    searchUsers(keyword) {
+        this.setState({loading: true});
+        setTimeout(() => {
+            axios
+                .get(`https://api.github.com/search/users?q=${keyword}`)
+                .then(res => this.setState({users: res.data.items, loading: false}));
+        }, 1000);
+        
+    }
 
-  searchUsers(keyword) {
-    this.setState({ loading: true });
+    clearUsers() {
+        this.setState({users: [] });
+    }
 
-    setTimeout(() => {
-      axios
-        .get(`https://api.github.com/users?q=${keyword}`)
-        .then((res) => this.setState({ users: res.data, loading: false }));
-    }, 3000);
-  }
-  clearUsers() {
-    this.setState({ users: [] });
-  }
+    setAlert(msg, type) {
+        this.setState({ alert: {msg,type} });
 
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Search
-          searchUsers={this.searchUsers}
-          clearUsers={this.clearUsers}
-          showClearButton={this.state.users.length > 0 ? true : false}
-        />
-        <Users users={this.state.users} loading={this.state.loading} />
-      </>
-    );
-  }
+        setTimeout(() => {
+            this.setState({ alert: null });
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <>
+                <Navbar />
+                <Alert alert= {this.state.alert}/>
+                <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClearButton = {this.state.users.length > 0? true:false}
+                    setAlert={this.setAlert}
+                    />
+                <Users users={this.state.users} loading={this.state.loading}/>
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,23 +1,24 @@
-import React, { Component } from "react";
-import User from "./user";
-import Loading from "./Loading";
+import React, { Component } from 'react'
+import User from './User'
+import Loading from './Loading'
 
-export class users extends Component {
-  render() {
-    if (this.props.loading) {
-      return <loading />;
-    } else {
-      return (
-        <div className="container mt-3">
-          <div className="row">
-            {this.state.users.map((user) => (
-              <User user={user} key={user.id} />
-            ))}
-          </div>
-        </div>
-      );
+class Users extends Component {   
+    render() {
+        if(this.props.loading) {
+          return <Loading />
+        } else {
+          return (
+            <div className="container mt-3">
+                <div className="row">
+                    {this.props.users.map(user => (
+                        <User user={user} key={user.id}/>
+                    ))}
+                </div>
+            </div>
+          ) 
+        }
+        
     }
-  }
 }
 
-export default users;
+export default Users
