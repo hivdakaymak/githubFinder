2c32b001ecc1af7af0bbcd2b47b5bff12cadcb5b
miraykaymak <miirayazz00@gmail.com>
2022-03-23 23:56:56 +0300

    React Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
-import App from './components/App'
+import App from './components/App';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HomePage = () => (
+    <div>Home Page</div>
+)
+const ContactPage = () => (
+    <div>Contact Page</div>
+)
+const ProductPage = () => (
+    <div>Products Page</div>
+)
+
+const NotFoundPage = () => (
+    <div>NotFound Page</div>
+)
+
+const routes = (
+    <BrowserRouter>
+    <Route exact path="/" component={HomePage} />
+    <Route path="/product" component={ProductPage} />
+    <Route component={NotFound} />
+
+
+
+    </BrowserRouter>
+
+
+
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
