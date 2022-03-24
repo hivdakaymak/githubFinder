d761bd2a2fd27004e49eb3fd275953924366d0cd
miraykaymak <miirayazz00@gmail.com>
2022-03-24 23:43:25 +0300

    Route Links

--- src/index.js
+++ src/index.js
@@ -1,35 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.scss';
-import App from './components/App';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.scss";
+import App from "./components/App";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 
-const HomePage = () => (
-    <div>Home Page</div>
-)
-const ContactPage = () => (
-    <div>Contact Page</div>
-)
-const ProductPage = () => (
-    <div>Products Page</div>
-)
-
-const NotFoundPage = () => (
-    <div>NotFound Page</div>
-)
-
-const routes = (
-    <BrowserRouter>
-    <Route exact path="/" component={HomePage} />
-    <Route path="/product" component={ProductPage} />
-    <Route component={NotFound} />
-
-
-
-    </BrowserRouter>
-
-
-
+const Nav = () => (
+  // <nav>
+  //   <Link to="/">Home</Link>
+  //   <Link to="/product">product</Link>
+  //   <Link to="/contact">contact</Link>
+  // </nav>
+  <nav>
+    <NavLink exact to="/" activeClassName="active">Home</NavLink>
+    <NavLink to="/product" activeClassName="active">product</NavLink>
+    <NavLink to="/contact" activeClassName="active">contact</NavLink>
+  </nav>
 );
 
-ReactDOM.render(routes, document.getElementById('root'));
+const Header = () => (
+  <header>
+    <h1>Github Finder</h1>
+  </header>
+);
+
+const HomePage = () => (
+  <>
+    <div>Home Page</div>
+  </>
+);
+
+const ContactPage = () => (
+  <>
+    <div>Contact Page</div>
+  </>
+);
+const ProductPage = () => (
+  <>
+    <div>Product Page</div>
+  </>
+);
+
+const NotFoundPage = () => (
+  <>
+    <div>404 Page</div>
+  </>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Header />
+
+    <Nav />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/product" component={ProductPage} />
+      <Route component={NotFound} />{" "}
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("root"));
