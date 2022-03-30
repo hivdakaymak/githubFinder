667bc5cfc90e0e68c191ece9ee9924dc5efa70f4
miraykaymak <miirayazz00@gmail.com>
2022-03-30 23:32:48 +0300

    updated user info

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,9 @@
-import React from "react";
-
+import React from 'react'
 
 const About = () => (
     <div className="container">
-      <h1>About Page </h1>
+        <h1>About Page</h1>
     </div>
-  );
-  
-  export default About;
+);
+
+export default About
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,79 +1,72 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+
 
 const Nav = () => (
-  // <nav>
-  //   <Link to="/">Home</Link>
-  //   <Link to="/product">product</Link>
-  //   <Link to="/contact">contact</Link>
-  // </nav>
-  <nav>
-    <NavLink exact to="/" activeClassName="active">
-      Home
-    </NavLink>
-    <NavLink exact to="/product" activeClassName="active">
-      product
-    </NavLink>
-    <NavLink to="/contact#mail" activeClassName="active">
-      contact
-    </NavLink>
-    <NavLink to="/product/12?orderby=price" activeClassName="active">
-      product details
-    </NavLink>
-  </nav>
+    <nav>
+        <NavLink exact to="/" activeClassName="active">Home</NavLink> |
+        <NavLink to="/contact#email" activeClassName="active">Contact</NavLink> |
+        <NavLink exact to="/products" activeClassName="active">Products</NavLink> |
+        <NavLink to="/products/12?orderby=price" activeClassName="active">Product Details</NavLink> |
+    </nav>
 );
 
 const Header = () => (
-  <header>
-    <h1>Github Finder</h1>
-  </header>
+    <header>
+        <h1>Github Finder</h1>
+    </header>
 );
 
 const HomePage = () => (
-  <>
-    <div>Home Page</div>
-  </>
+    <>
+        <div>Home Page</div>
+    </>
 );
 
 const ContactPage = (props) => {
-  return (
-    <>
-      <div>Contact Page</div>
-    </>
-  );
-};
-const ProductPage = () => (
-  <>
-    <div>Product Page</div>
-  </>
-);
+     console.log(props);
+    return (
+        <>
+            <div>Contact Page</div>
+        </>
+    );
+}
 
-const NotFoundPage = () => (
-  <>
-    <div>404 Page</div>
-  </>
+const ProductsPage = () => (
+    <>
+        <div>Products Page</div>
+    </>
 );
 
 const ProductDetailsPage = (props) => {
-  return (
-    <>
-      <div>ProductDetailsPage Page</div>
-      <div>{props.match.params.id}</div>
-    </>
-  );
+    console.log(props);
+    return (
+        <>
+            <div>ProductDetails Page</div>
+            <div>{props.match.params.id}</div>
+        </>
+    );
 };
 
-const AppRouter = () => (
-  <BrowserRouter>
+const NotFoundPage = () => (
+    <>
+        <div>NotFound Page</div>
+    </>
+);
+
+
+const AppRouter = ()=> (
+    <BrowserRouter>
     <Header />
     <Nav />
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/product" component={ProductPage} />
-      <Route exact path="/product/:id" component={ProductDetailsPage} />
-      <Route component={NotFound} />{" "}
+        <Route exact path="/" component={ HomePage } />
+        <Route path="/contact" component={ ContactPage } />
+        <Route exact path="/products" component={ ProductsPage } />
+        <Route path="/products/:id" component={ ProductDetailsPage } />
+        <Route component={ NotFoundPage } />
     </Switch>
-  </BrowserRouter>
+</BrowserRouter>
 );
 
 export default AppRouter;
